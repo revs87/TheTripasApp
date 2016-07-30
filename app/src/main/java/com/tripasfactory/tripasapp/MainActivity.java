7a1cded5bc868dd64ca16deb0cca167ddc6c52c2
Rui Vieira <revs87@msn.com>
2016-07-30 02:18:44 +0100

    Map support added

--- app/src/main/java/com/tripasfactory/tripasapp/MainActivity.java
+++ app/src/main/java/com/tripasfactory/tripasapp/MainActivity.java
@@ -1,15 +1,48 @@
 package com.tripasfactory.tripasapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    private SupportMapFragment supportMapFragment;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpMapIfNeeded();
+    }
 
+    private void setUpMapIfNeeded() {
+        if (mMap != null) {
+            return;
+        }
+        supportMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).newInstance();
+        if (supportMapFragment == null) {
+            return;
+        }
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                if (mMap == null) {
+                    initializeMap();
+                }
+            }
+        });
+    }
+
+    private void initializeMap() {
+        // Initialize map options. For example:
+        // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
