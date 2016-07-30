8c19f19c63d78c2cdd3bd949b367bac90d5a4499
Rui Vieira <revs87@msn.com>
2016-07-30 03:55:49 +0100

    New GMaps version usage Runtime permissions applied

--- app/src/main/java/com/tripasfactory/tripasapp/MainActivity.java
+++ app/src/main/java/com/tripasfactory/tripasapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.tripasfactory.tripasapp;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends FragmentActivity {
+    private final int PERMISSION_CODE = 100;
 
-    private SupportMapFragment supportMapFragment;
     private GoogleMap mMap;
 
     @Override
@@ -21,28 +26,76 @@ public class MainActivity extends FragmentActivity {
     }
 
     private void setUpMapIfNeeded() {
-        if (mMap != null) {
-            return;
-        }
-        supportMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).newInstance();
-        if (supportMapFragment == null) {
-            return;
-        }
-
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
-                if (mMap == null) {
-                    initializeMap();
+                if (mMap != null) {
+                    setupMap();
                 }
             }
         });
     }
 
-    private void initializeMap() {
-        // Initialize map options. For example:
-        // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    private void setupMap() {
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(false);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setTiltGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        if (getMyLocationSharedPreference() ||
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+            setMyLocationButton();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (permissions.length == 2 &&
+                    Manifest.permission.ACCESS_FINE_LOCATION.equals(permissions[0]) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    Manifest.permission.ACCESS_COARSE_LOCATION.equals(permissions[1]) &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                setMyLocationSharedPreference(true);
+                setMyLocationButton();
+            } else {
+                // Permission was denied. Display an error message.
+                setMyLocationSharedPreference(false);
+            }
+        }
+    }
+
+    private void setMyLocationButton() {
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                return false;
+            }
+        });
+    }
+
+    private boolean getMyLocationSharedPreference() {
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        return prefs.getBoolean(String.valueOf(PERMISSION_CODE), false);
+    }
+
+    private void setMyLocationSharedPreference(boolean allowed) {
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putBoolean(String.valueOf(PERMISSION_CODE), allowed);
+        editor.commit();
+    }
+
 }
